79119787fca9b0685e77df177c7edc6540b234ae
WMatias-Dev <williamlopes0753@gmail.com>
2026-07-31 23:42:44 -0300

    feat(documents): adiciona classe DocumentRepository para operações de banco
    Implementação do padrão Repository para a entidade Document, isolando a lógica de acesso ao banco de dados (SQLAlchemy) das regras de negócio (Services/Routers).

---
+++ backend/app/repositories/document_repository.py
@@ -0,0 +1,34 @@
+import uuid
+from typing import List, Optional
+from sqlalchemy.orm import Session
+from app.database.models.document import Document
+from app.schemas.document_schema import DocumentCreate
+
+class DocumentRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    #Relacionando os dois dados com uma identidade segura
+    def create(self, document_in: DocumentCreate, owner_id: uuid.UUID) -> Document:
+        db_document = Document(
+            title=document_in.title,
+            content=document_in.content,
+            owner_id=owner_id
+        )
+        self.db.add(db_document)
+        self.db.commit()
+        self.db.refresh(db_document)
+        return db_document
+
+    def get_by_id(self, document_id: uuid.UUID) -> Optional[Document]:
+        # Apenas busca pelo ID, não pergunta de quem é
+        return self.db.query(Document).filter(Document.id == document_id).first()
+
+    def get_all_by_owner(self, owner_id: uuid.UUID) -> List[Document]:
+        # Retorna todos os documentos que pertencem a um usuário específico
+        return self.db.query(Document).filter(Document.owner_id == owner_id).all()
+
+    def delete(self, document: Document) -> None:
+        # Recebe a entidade já validada pelo serviço e apenas deleta
+        self.db.delete(document)
+        self.db.commit()
